d32a1dce81a4a49aa5e579cc53b231d6a2079017
maxx <m.petrykin@gmx.de>
2025-12-30 22:10:32 +0100

    turnstile urls fix

--- config/urls.py
+++ config/urls.py
@@ -1,24 +1,28 @@
+from django.urls import path, include, reverse
+from django.shortcuts import redirect
 from django.contrib import admin
-from django.urls import include, path
-from django.http import HttpResponseRedirect
-from allauth.socialaccount.providers.google.views import oauth2_login
 
 from apps.accounts.views_turnstile import google_login_gate
+from allauth.socialaccount.providers.google.views import oauth2_login
 
+
+def root(request):
+    return redirect(f"{reverse('google_login_gate')}?next=/")
 
 def google_only_login(request):
     next_url = request.GET.get("next", "/")
-    return HttpResponseRedirect(f"/accounts/google/login/?next={next_url}")
-
+    return redirect(f"{reverse('google_login_gate')}?next={next_url}")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    path("", google_login_gate),
+    path("", root),
 
     path("accounts/login/", google_only_login),
     path("accounts/google/login/", google_login_gate, name="google_login_gate"),
+
     path("accounts/google/oauth/", oauth2_login, name="google_oauth_login"),
+
     path("accounts/", include("allauth.urls")),
 
     path("app/", include("apps.accounts.urls")),
